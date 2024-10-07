2fcc5efe88a8c3fb2226475eab9c6fe3fe75412e
Asega-bryan07 <Almasibryan7@gmail.com>
2024-10-08 01:32:28 +0300

    Add: Screens & Component

--- App.tsx
+++ App.tsx
@@ -3,8 +3,11 @@ import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
+import MyTasksScreen from './screens/MyTasksScreen';
+import EditTaskScreen from './screens/EditTaskScreen';
+import AddNewTaskScreen from './screens/AddNewTaskScreen';
 
-const stack = createStackNavigator();
+const Stack = createStackNavigator();
 
 export default function App(){
   return (
@@ -12,8 +15,8 @@ export default function App(){
       <Stack.Navigator initialRouteName="Home">
         {/* screens  */}
         <Stack.Screen name="MyTasks" component={MyTasksScreen} options={{ title: 'My Tasks' }} /> 
-        <Stack.Screen name="EditTask" component={EditTaskScreen} options={{ title: 'Edit Tasks' }} />
         <Stack.Screen name="AddNewTask" component={AddNewTaskScreen} options={{ title: 'Create New Tasks' }} />
+        <Stack.Screen name="EditTask" component={EditTaskScreen} options={{ title: 'Edit Tasks' }} />
 
       </Stack.Navigator>
     </NavigationContainer>
