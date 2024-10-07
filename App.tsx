6646231d50b08c33184e0a64041168bec59ad6fb
Asega-bryan07 <Almasibryan7@gmail.com>
2024-10-08 00:31:14 +0300

    Add: Screens

--- App.tsx
+++ App.tsx
@@ -1,20 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
+const stack = createStackNavigator();
+
+export default function App(){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app! </Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        {/* screens  */}
+        <Stack.Screen name="MyTasks" component={MyTasksScreen} options={{ title: 'My Tasks' }} /> 
+        <Stack.Screen name="EditTask" component={EditTaskScreen} options={{ title: 'Edit Tasks' }} />
+        <Stack.Screen name="AddNewTask" component={AddNewTaskScreen} options={{ title: 'Create New Tasks' }} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FFF',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
